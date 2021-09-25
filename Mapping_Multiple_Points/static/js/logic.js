5158e130df141c9aa4b9c6a326bfe2cf4a2e3a87
Francisco170594 <francisco941705@gmail.com>
2021-09-24 20:11:00 -0500

    Create multiple markers

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -15,22 +15,27 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-var darkmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/dark-v10',
-    accessToken: API_KEY
-  });
 
-darkmap.addTo(map);  
+// Get data from cities.js
+let cityData = cities;
 
 
+  // Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        color: 'orange',
+        fillColor: '#ffa500',
+        weight: 4,
+        radius: city.population / 200000
+    })
+    .bindPopup("<h2>" + city.city + "," + city.state + "</h2> <hr> <h3> Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+    
+   });
 
-// Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+   
 
-L.circleMarker([34.0522, -118.2437], {
-    color: 'black',
-    fillColor: '#ffffa1',
-    radius: 300
- }).addTo(map);
+
+
+
