fabba72bb4ccbf9dde2b26d9cfab45950ba58d51
Francisco170594 <francisco941705@gmail.com>
2021-09-24 21:00:10 -0500

    Playing with Polyline attributes

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -18,15 +18,45 @@ L.polyline(line, {
   }).addTo(map);
 
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Create polyline from JFK to
+
+let newLine = [
+    [37.6213, -122.3790],
+    [30.1974292, -97.6663058],
+    [43.67771760000001, -79.62481969999999],
+    [40.641766, -73.780968]  
+
+];
+
+// Create polyline base oon newLine array
+L.polyline(newLine,{
+    color: 'blue',
+    weight: 4,
+    opacity: 0.5,
+    dashArray: 10
+}).addTo(map);
+
+
+  // We create the tile layer that will be the background of our map.
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+satellite.addTo(map);
+
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        accessToken: API_KEY
+    });
+
+// Add layer to map
+light.addTo(map);
+
+
 
 
 
